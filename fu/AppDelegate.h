06908a039b824b9679256e8d93abd0e0ca134d61
Klesh Wong <klesh@3yc.io>
2017-01-05 20:32:55 +0800

    Initial Commit

---
+++ fu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fu
+//
+//  Created by Klesh Wong on 05/01/2017.
+//  Copyright © 2017 Klesh Wong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ fu/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  fu
+//
+//  Created by Klesh Wong on 05/01/2017.
+//  Copyright © 2017 Klesh Wong. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
