e992b9e5a0fa9b3ef4d31af599718cf369ceb61d
Klesh Wong <klesh@qq.com>
2017-04-21 18:47:54 +0800

    [feature] about dialog, organize file by date as folder for sftp

---
+++ fu/src/ui/aboutform.cpp
@@ -0,0 +1,47 @@
+#ifndef UI_ABOUT
+#define UI_ABOUT
+
+#include <wx/wx.h>
+#include <wx/hyperlink.h>
+#include "../core/file.cpp"
+#include "common.cpp"
+
+class AboutForm : public wxDialog
+{
+private:
+    wxStaticBitmap* _icon;
+    wxHyperlinkCtrl* _link;
+    wxStaticText* _version;
+    wxButton* _close;
+    
+public:
+    AboutForm() : wxDialog(NULL, wxID_ANY, "About", wxPoint(-1, -1), wxSize(400, 300))
+    {
+        SetIcon(UICommon::Get().Icon());
+        wxImage img = File::Thumbnailize(wxImage("/home/klesh/Projects/Study/fu/fu/resources/fu.png", wxBITMAP_TYPE_PNG), 100, 100);
+        _icon = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap);
+        _icon->SetBitmap(wxBitmap(img));
+        
+        _link = new wxHyperlinkCtrl(this, wxID_ANY, "https://github.com/klesh/fu", "https://github.com/klesh/fu");
+        _version = new wxStaticText(this, wxID_ANY, "version: 1.3");
+        _close = new wxButton(this, wxID_ANY, "Close");
+        Bind(wxEVT_BUTTON, &AboutForm::OnCloseClicked, this, wxID_ANY);
+
+        
+        wxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+        sizer->AddStretchSpacer();
+        sizer->Add(_icon, wxSizerFlags().Border(wxALL, 5).Center());
+        sizer->Add(_link, wxSizerFlags().Border(wxALL, 5).Center());
+        sizer->Add(_version, wxSizerFlags().Border(wxALL, 5).Center());
+        sizer->Add(_close, wxSizerFlags().Border(wxALL, 5).Center());
+        sizer->AddStretchSpacer();
+        SetSizer(sizer);
+    }
+
+    void OnCloseClicked(wxCommandEvent &evt)
+    {
+        this->Hide();
+    }
+};
+
+#endif
